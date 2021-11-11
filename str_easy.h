b71f90fb871bb5c8ada9c939befc6b8ecef40aaa
a111r <92301153+a111r@users.noreply.github.com>
2021-11-11 10:37:15 +0300

    Update str_easy.h

--- str_easy.h
+++ str_easy.h
@@ -6,7 +6,7 @@
 using namespace std;
 
 string itc_hello_str(string name);
-long long itc_len(string str);
+long itc_len(string str);
 void itc_print_copy_str(string str, int number);
 void itc_first_end_three(string str);
 int itc_count_char_in_str(char ch, string str);
