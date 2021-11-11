a11f6b55896d169e3b7295df389b240fc1872ec2
a111r <92301153+a111r@users.noreply.github.com>
2021-11-11 09:01:33 +0300

    Update func2.cpp

--- func2.cpp
+++ func2.cpp
@@ -31,13 +31,18 @@ int itc_find_str(string str1, string str2)
 
 string itc_three_str(string str1, string str2, string str3)
 {
-    long long i;
-    i = 0;
-    while (itc_find_str(itc_slice_str(str1, i , itc_len(str1)), str2) != -1){
-            long long start = itc_find_str(itc_slice_str(str1, i , itc_len(str1)), str2);
-            str1 = itc_slice_str(str1, 0, start) + str3 + itc_slice_str(str1, start + itc_len(str2), itc_len(str1));
+    int i = 0;
+    int start;
+    start = itc_find_str(str1, str2);
+    while (str1[i] != '\0'){
+        start = itc_find_str(str1, str2);
+        if (start == -1) 
+            i += 1;
+        else 
+        {
+        str1 = itc_slice_str(str1, 0, start - 1) + str3 + itc_slice_str(str1, start + itc_len(str2), itc_len(str1));
+        i = start + itc_len(str2);
         }
-        return str1;
     }
-
+    return str1;
 }
